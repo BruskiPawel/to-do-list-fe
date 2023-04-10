245d4613b83be2733204691e2e5f475150efd6b5
Penknife92 <112814704+Penknife92@users.noreply.github.com>
2023-04-10 23:50:37 +0200

    trying drag and drop

--- src/components/TaskBoardComponent.js
+++ src/components/TaskBoardComponent.js
@@ -6,6 +6,7 @@ import classes from "./TaskBoardComponent.module.css";
 
 function TaskBoardComponent() {
   const [tasks, setTasks] = useState(null);
+  const [draggedTask, setDraggedTask] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
 
   const getAllTasks = (taskArr) => {
@@ -29,17 +30,69 @@ function TaskBoardComponent() {
     fetchTasks();
   }, []);
 
+  const handleDragStart = (e, item) => {
+    setDraggedTask(item);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDragEnter = (e, task) => {
+    const target = e.target.closest("div");
+    if (target && target.classList.contains(classes.task)) {
+      target.classList.add(classes.dragged);
+     }
+  };
+
+  const handleDragLeave = (e) => {
+    const target = e.target.closest("div");
+    if (target && target.classList.contains(classes.dragged)) {
+      
+    }
+    //target.classList.remove(classes.dragged);
+  };
+
+  const handleDrop = (e) => {
+    console.log("drop");
+    const movedTask = tasks.find(
+      (task) => task.id === parseInt(draggedTask.id)
+    );
+    const targetTask = tasks.find(
+      (task) => task.id === parseInt(e.target.closest("div").id)
+    );
+    const newList = tasks.filter((task) => task.id !== movedTask.id);
+
+    newList.splice(
+      newList.findIndex((task) => task.id === targetTask.id),
+      0,
+      movedTask
+    );
+    setTasks(newList);
+    e.preventDefault();
+  };
+
   return (
     <div>
       <NewTaskComponent onSubmit={fetch} />
       <div className={classes["tasks-container"]}>
         {tasks ? (
           tasks.map((task) => (
-            <div className={classes.task} key={task.id}>
+            <div
+              className={classes.task}
+              key={task.id}
+              id={task.id}
+              draggable
+              onDragStart={(e) => handleDragStart(e, task)}
+              onDragOver={handleDragOver}
+              onDragEnter={(e) => handleDragEnter(e, task)}
+              onDragLeave={(e) => handleDragLeave(e, task)}
+              onDrop={handleDrop}
+            >
               <h2>{task.title}</h2>
               <h3>{task.content}</h3>
               <p>{task.id}</p>
-              <DeleteTaskComponent/>
+              <DeleteTaskComponent />
             </div>
           ))
         ) : (
